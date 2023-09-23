bb788d3ab7f73cc12de9af7441e720e64eb648c1
Athoy <athoykanti.roy1612@gmail.com>
2023-09-23 21:53:30 +0600

    Advanced-Programming-with-.Net

---
+++ Portfolio_Athoy/Portfolio_Athoy/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio_Athoy.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            return View();
+        }
+
+        public ActionResult Project()
+        {
+            return View();
+        }
+        public ActionResult Reference()
+        {
+            return View();
+        }
+    }
+}
